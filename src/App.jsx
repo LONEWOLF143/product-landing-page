40eb8b13e8c2f4290747ce3e8c2535c6b5ba4ec7
LONEWOLF143 <twaha2030@gmail.com>
2023-08-24 12:43:52 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main className='relative'>
+      nav
+      <section className='xl:padding-1 wide:padding-r padding-b'>
+        Hero
+      </section>
+      <section className='padding'>ppopular product</section>
+      <section className='padding'>superQuality</section>
+      <section className='padding-x py-10'>services</section>
+      <section className='padding'>especial offer</section>
+      <section className='bg-pale-blue padding'>customer reviews</section>
+      <section className='padding-x sm:py-32 py-16 w-full'>subscribe</section>
+      <section className=' bg-black padding-x padding-t pb-8'>footer</section>
+
+    </main>
+  )
+}
+
+export default App
